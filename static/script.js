d15193595305789bf76c31de1176d34bf31fcddc
Carlos Navas <carlosnavas1991@gmail.com>
2016-04-05 17:52:37 +0200

    Updating animations

--- static/script.js
+++ static/script.js
@@ -1,25 +1,37 @@
 
 var win = $(this);
-
+var contentContainer = $('.content-container');
 
 win.on('click', '.sign-in', function(){
 
   $('.inevio-logo').transition({
     'margin-top': '156px'
-  },600,function(){});
+  },1500,function(){});
 
   $('.slogan').transition({
-    'left': '-100%'
-  },300,function(){
+    'left': '-50%'
+  },1500,function(){
 
     $(this).hide();
     $('.login.inputs').css('display','block').transition({
-      'left' : '0%'
-    },300);
+      'left' : '50%'
+    },1500);
+
+    contentContainer.transition({
+      'height' : '151px'
+    },1500);
 
   });
 
-  $('.sign-up').css({'margin-right':'0px'});
+  $('.login-buttons').transition({
+    'margin-top':'144px'
+  },1500,function(){
+    $(this).transition({
+      'margin-top':'73px'
+    },1500);
+  });
+
+  /*$('.sign-up').css({'margin-right':'0px'});
 
   $('.sign-up').transition({
     'opacity': '0'
@@ -43,7 +55,7 @@ win.on('click', '.sign-in', function(){
     'opacity' : '0'
   },300, function(){
     $(this).hide();
-  });
+  });*/
 
 
 
@@ -51,7 +63,7 @@ win.on('click', '.sign-in', function(){
 
 .on('click', '.sign-up', function(){
 
-  $('.inevio-logo').transition({
+  /*$('.inevio-logo').transition({
     'margin-top': '156px'
   },600,function(){});
 
@@ -87,6 +99,32 @@ win.on('click', '.sign-in', function(){
     'opacity' : '0'
   },300, function(){
     $(this).hide();
+  });*/
+
+})
+
+.on('click', '.forgot', function(){
+
+  /*$('.passwd-recover').show();
+
+  $('.inputs.login').transition({
+    'margin-top':'83px',
+    'height':'93px'
+  },600,function(){
+    $('.forgot').hide();
+    $(this).find('.username').removeClass('username').addClass('email').find('input').attr('placeholder','Email').val('' );
   });
+
+  $('.inputs.login .password').transition({
+    'opacity':'0'
+  },600,function(){
+    $(this).hide();
+  });
+
+  $('.login-buttons').transition({
+    'margin-top':'48px'
+  },600,function(){
+    $(this).find('.sign-in span').text('Recover');
+  });*/
 
 });
