ea951d3f05f92e64ec5a57b0472ff6612b20b703
Carlos Navas <carlosnavas1991@gmail.com>
2016-07-12 17:41:02 +0200

    All merged

--- static/script.js
+++ static/script.js
@@ -1,34 +1,46 @@
-var win = $(this);
+var win = $('body');
 var contentContainer = $('.content-container');
+var bodyWidth = win.css('width');
+var bodyHeight = win.css('height');
+var initialYlogo = parseInt( $('.login-screen .inevio-logo.white').css('transform').split(',')[5] );
+var initialMarginTopLogo = parseInt( $('.content-container').css('margin-top') ) || parseInt( $('.content-container').css('top') ) ;
+var initialTopLogo = parseInt( $('.login-screen .inevio-logo.dark').css('top') );
 var initialMarginRightBtn = $('.login-buttons .accept').css('margin-right');
 var initialMarginLeftLogo = $('.login-screen .inevio-logo.dark').css('margin-left');
 var initialButtonWidth = $('.login-buttons .accept').css('width');
 var initialHeight = contentContainer.height();
 var inevioLogo = $('.inevio-logo');
 var backButton = $('.back-button');
-var initialMarginTopLogo = inevioLogo.css('top');
 var loginStage = 0;// 0 = intro, 1=sign-in, 2=sign-up, 3=recover 4=recover-success, -1 transition
-var mc;
+var hammerJS;
 var hammerLoaded = false;
 var menuMode = false;
 var menuStage = 1;
-var transitioning = false;
+var lowResMode = false;
 var used = false;
-/*
-var intervalo = setInterval( function(){
+var transitioning = false;
+
+if( parseInt( win.css('width') ) < 360 ){
+  lowResMode = true;
+}
+
+/*var intervalo = setInterval( function(){
+
   if( typeof Hammer !== 'undefined' ){
     clearInterval( intervalo );
-    console.log('cargo');
     hammerLoaded = true;
-    mc = new Hammer(win[0] , {
+    hammerJS = new Hammer(win[0] , {
       domEvents:true
     });
+    hammerJS.get('swipe').set({ direction: Hammer.DIRECTION_ALL })
   }
-},50);
-*/
+},50);*/
+
 var createCache = function(){
 
-  initialMarginTopLogo = inevioLogo.css('top');
+  //initialYlogo = parseInt( $('.login-screen .inevio-logo.white').css('transform').split(',')[5] );
+  initialMarginTopLogo = parseInt( $('.content-container').css('margin-top') ) || parseInt( $('.content-container').css('top') );
+  initialTopLogo = parseInt( $('.login-screen .inevio-logo.dark').css('top') );
   initialMarginRightBtn = $('.login-buttons .accept').css('margin-right');
   initialMarginLeftLogo = $('.login-screen .inevio-logo.dark').css('margin-left');
   initialButtonWidth = $('.login-buttons .accept').css('width');
@@ -36,25 +48,23 @@ var createCache = function(){
 
 }
 
-
 var back = function( stage ){
 
-  $('.input.error').removeClass('error');
-
+  $('.login-screen .error').removeClass('error');
   if( stage == 1 ){
 
     loginStage = -1;
-    inevioLogo.transition({
-      'top': initialMarginTopLogo,
+    $('.login-screen .inevio-logo').transition({
+      'top': initialTopLogo,
     },500);
 
     $('.login.inputs').transition({
-      'left': '100%'
+      'x': bodyWidth
     },250,function(){
 
       $(this).hide();
       $('.slogan').show().transition({
-        'left' : '0%'
+        'x' : '0%'
       },250);
 
     });
@@ -85,17 +95,17 @@ var back = function( stage ){
   }else if( stage == 2 ){
 
     loginStage = -1;
-    inevioLogo.transition({
-      'top': initialMarginTopLogo,
+    $('.login-screen .inevio-logo').transition({
+      'top': initialTopLogo,
     },500,function(){});
 
     $('.inputs.register').transition({
-      'left': '100%'
+      'x': bodyWidth
     },250,function(){
 
       $(this).hide();
       $('.slogan').show().transition({
-        'left' : '0%'
+        'x' : '0%'
       },250);
 
     });
@@ -156,18 +166,18 @@ var back = function( stage ){
     },500);
 
     $('.recover-passwd-success').transition({
-      'left' : '100%'
+      'x' : bodyWidth
     },250,function(){
 
       $(this).hide();
       $('.inputs.login').show().transition({
-        'left' : '0'
+        'x' : '0'
       },250,function(){
         loginStage = 3;
       });
 
       $('.login-buttons').show().transition({
-        'left' : '0%'
+        'x' : '0%'
       },250);
 
     });
@@ -180,30 +190,32 @@ var menuSwipe = function( value ){
 
   if( !transitioning ){
 
+    console.log(value);
     if( value === 1 ){
 
       if( menuStage !== 3 ){
 
         transitioning = true;
-        menuStage++;
 
-        $('.menu-content.active').stop().clearQueue().transition({
+        $('.menu-content.active').transition({
           'left' : '-100%'
         },500,function(){
 
+          menuStage++;
           $(this).removeClass('active');
           $(this).hide();
           $('.bullet.active').removeClass('active');
           $('.bullet-' + menuStage).addClass('active');
           transitioning = false;
 
-        })
+        });
 
-        $('.menu-content-' + menuStage).show().stop().clearQueue().transition({
+        $('.menu-content-' + menuStage).show().transition({
           'left' : '0'
         },500,function(){
           $(this).addClass('active');
-        })
+        });
+
 
       }
 
@@ -213,7 +225,7 @@ var menuSwipe = function( value ){
 
         transitioning = true;
         menuStage--;
-        $('.menu-content.active').stop().clearQueue().transition({
+        $('.menu-content.active').transition({
           'left' : '100%'
         },500,function(){
 
@@ -223,13 +235,13 @@ var menuSwipe = function( value ){
           $('.bullet-' + menuStage).addClass('active');
           transitioning = false;
 
-        })
+        });
 
-        $('.menu-content-' + menuStage).show().stop().clearQueue().transition({
-          'left' : '0'
-        },500,function(){
-          $(this).addClass('active');
-        })
+        $('.menu-content-' + menuStage).show().transition({
+           'left' : '0'
+         },500,function(){
+           $(this).addClass('active');
+         })
 
       }
 
@@ -237,29 +249,30 @@ var menuSwipe = function( value ){
 
   }
 
+
 }
 
-$('.sign-in').on('click', function(){
+win.on('click', '.sign-in', function(){
 
   if( !used ){
-    used = true;
-    createCache();
+   used = true;
+   createCache();
   }
 
   if( loginStage == 0 ){
 
     loginStage = -1;
-    inevioLogo.transition({
+    $('.login-screen .inevio-logo').transition({
       'top': '0px',
     },500);
 
     $('.slogan').transition({
-      'left': '-100%'
+      'x': '-' + bodyWidth
     },250,function(){
 
       $(this).hide();
       $('.login.inputs').show().transition({
-        'left' : '0%'
+        'x' : '0'
       },250);
 
     });
@@ -271,7 +284,7 @@ $('.sign-in').on('click', function(){
     });
 
     $(this).transition({
-      'width': '317px',
+      'width': '100%',
       'background-color': '#60b25e',
       'border': '2px solid #60b25e'
     },500,function(){
@@ -301,76 +314,36 @@ $('.sign-in').on('click', function(){
       $('.login.inputs .input').addClass('error');
     }
 
-  }else if( loginStage == 3 ){
-
-    console.log('recover');
-    if( $('.login.inputs .email').find('input').val() === 'a' ){
-
-      loginStage = -1;
-      $('.login.inputs .email').removeClass('error');
-      $('.passwd-recover').removeClass('error').text('Recover password');
-
-      $('.passwd-recover').transition({
-        'opacity' : '0'
-      },500,function(){
-        $(this).hide();
-      });
-
-      $('.inputs.login').transition({
-        'left' : '-100%'
-      },250,function(){
-
-        $(this).hide();
-        $('.recover-passwd-success').show().transition({
-          'left' : '0'
-        },250,function(){
-          loginStage = 4;
-        })
-
-      });
-
-      $('.login-buttons').transition({
-        'left' : '-200%'
-      },500,function(){
-        $(this).hide();
-      });
-
-    }else{
-      $('.login.inputs .email').addClass('error');
-      $('.passwd-recover').addClass('error').text('Email incorrecto');
-    }
-
   }
 
-});
+})
 
-$('.sign-up').on('click', function(){
+.on('click', '.sign-up', function(){
 
   if( !used ){
-    used = true;
-    createCache();
+   used = true;
+   createCache();
   }
 
   if( loginStage == 0 ){
 
     loginStage = -1;
-    inevioLogo.transition({
-      'top': '0px',
+    $('.login-screen .inevio-logo').transition({
+      'top': '0',
     },500);
 
     $('.slogan').transition({
-      'left': '-100%'
+      'x': '-' + bodyWidth
     },250,function(){
 
       $(this).hide();
       $('.inputs.register').show().transition({
-        'left' : '0%'
+        'x' : '0%'
       },250);
 
     });
 
-    initialHeight = contentContainer.height();
-
+    console.log(initialHeight);
     contentContainer.transition({
       'height' : initialHeight + 73
     },500);
@@ -378,11 +351,10 @@ $('.sign-up').on('click', function(){
     $('.sign-in').hide();
 
     $(this).transition({
-      'width': '317px',
+      'width': '100%',
       'margin-right':'0px',
       'background-color': '#60b25e',
-      'border': 'none',
-      'height': '58px'
+      'border': 'none'
     },500);
 
     $('.more').transition({
@@ -400,17 +372,23 @@ $('.sign-up').on('click', function(){
 
   }
 
-});
+})
 
-$('.forgot').on('click', function(){
+.on('click', '.forgot', function(){
 
+  $('.login-screen .error').removeClass('error');
   loginStage = -1;
   $('.passwd-recover').show().transition({
     'opacity': '1'
   },500);
 
+  var height = 360;
+  if( lowResMode ){
+    height = 265;
+  }
+
   contentContainer.transition({
-    'height' : '360px'
+    'height' : height
   },500);
 
   $('.inputs.login .password').transition({
@@ -427,26 +405,64 @@ $('.forgot').on('click', function(){
 
 })
 
-backButton.on('click', function(){
+.on('click', '.recover', function(){
 
-  back( loginStage );
+  console.log('recover');
+  if( $('.login.inputs .email').find('input').val() === 'a' ){
 
-});
+    loginStage = -1;
+    $('.login.inputs .email').removeClass('error');
+    $('.passwd-recover').removeClass('error').text('Recover password');
 
-$('.more').on('click', function(){
+    $('.passwd-recover').transition({
+      'opacity' : '0'
+    },500,function(){
+      $(this).hide();
+    });
 
-  if( !used ){
-    used = true;
-    createCache();
+    $('.inputs.login').transition({
+      'x' : '-' + bodyWidth
+    },250,function(){
+
+      $(this).hide();
+      $('.recover-passwd-success').show().transition({
+        'x' : '0',
+        'y' : '50px'
+      },250,function(){
+        loginStage = 4;
+      })
+
+    });
+
+    $('.login-buttons').transition({
+      'x' : '-' + bodyWidth
+    },500,function(){
+      $(this).hide();
+    });
+
+  }else{
+    $('.login.inputs .email').addClass('error');
+    $('.passwd-recover').addClass('error').text('Email incorrecto');
   }
 
-  initialMarginLeftLogo = $('.login-screen .inevio-logo.dark').css('margin-left');
+})
+
+.on('click', '.back-button', function(){
+  back( loginStage );
+})
+
+.on('click', '.more', function(){
+
+  if( !used ){
+   used = true;
+   createCache();
+  }
+
   if( $('.start .inevio-logo').length == 0 ){
-    $('.start').append( inevioLogo.clone().css({'top':'185px','background-size':'cover', 'margin-left': $('.inevio-logo.dark').css('margin-left') }) );
+    $('.start').append( inevioLogo.clone().css({'top': (initialMarginTopLogo + initialTopLogo),'background-size':'cover', 'margin-left':'0px', 'left': $('.inevio-logo.dark').css('margin-left') }).show() );
   }
 
   $('.start .inevio-logo.white').show().transition({
-    'margin-left' : '0px',
     'width': '108px',
     'height': '21px',
     'top': '20px',
@@ -459,7 +475,6 @@ $('.more').on('click', function(){
   });
 
   $('.start .inevio-logo.dark').show().transition({
-    'margin-left' : '0px',
     'width': '108px',
     'height': '21px',
     'top': '20px',
@@ -471,26 +486,19 @@ $('.more').on('click', function(){
 
   $('.login-screen .inevio-logo').hide();
 
-  $('.login-screen').transition({
-    'bottom' : '100%'
-  },1000,function(){
-    $(this).hide();
-  });
-
   $('.menu-screen').show().transition({
     'bottom' : '0'
   },1000);
 
-});
+})
 
-$('.less').on('click', function(){
+.on('click', '.less', function(){
 
   $('.start .inevio-logo.white').show().transition({
-    'margin-left' : initialMarginLeftLogo,
-    'width': '251px',
-    'height': '49px',
-    'top': '185px',
-    'left': '0',
+    'width': $('.login-screen .inevio-logo.white').css('width'),
+    'height': $('.login-screen .inevio-logo.white').css('height'),
+    'left': initialMarginLeftLogo,
+    'top': initialMarginTopLogo + initialTopLogo,
     'opacity' : '1'
   },1000,function(){
     menuMode = false;
@@ -499,19 +507,16 @@ $('.less').on('click', function(){
   });
 
   $('.start .inevio-logo.dark').show().transition({
-    'margin-left' : initialMarginLeftLogo,
-    'width': '251px',
-    'height': '49px',
-    'top': '185px',
-    'left': '0',
+    'width': $('.login-screen .inevio-logo.white').css('width'),
+    'height': $('.login-screen .inevio-logo.white').css('height'),
+    'top': initialMarginTopLogo + initialTopLogo,
+    'left': initialMarginLeftLogo,
     'opacity' : '0'
   },1000,function(){
     $(this).hide();
   });
 
-  $('.login-screen').show().transition({
-    'bottom' : '0'
-  },1000);
+  $('.menu-screen .header figure').hide();
 
   $('.menu-screen').transition({
     'bottom' : '-100%'
@@ -519,24 +524,22 @@ $('.less').on('click', function(){
     $(this).hide();
   });
 
-  $('.menu-screen .header figure').hide();
-
-});
-
-$('body').on( 'swiperight', function(){
-
-  if( menuMode ){
-    menuSwipe(-1);
-  }else{
-    backButton.click();
-  }
-
 })
 
 .on( 'swipeleft', function(){
 
   if( menuMode ){
     menuSwipe(1);
+  }
+
+})
+
+.on( 'swiperight', function(){
+
+  if( menuMode ){
+    menuSwipe(-1);
+  }else{
+    backButton.click();
   }
 
 })
@@ -556,22 +559,3 @@ $('body').on( 'swiperight', function(){
   }
 
 });
-
-/*$('.bullet-2').on('click', function(){
-
-  if( !menuMode ){
-    backButton.click();
-  }else{
-    menuSwipe(-1);
-  }
-
-});
-
-$('.bullet-3').on('click', function(){
-
-  if( !menuMode ){
-    backButton.click();
-  }else{
-    menuSwipe(1);
-  }
-});*/
